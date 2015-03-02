bede5cb492ba096b9086f4b11acf2e88361932af
Arkadiy <a.sedelnikov@gmail.com>
2015-03-02 16:16:50 +0600

    Добавил лицензионный ключ, почистил код.

--- jlnodoubles.php
+++ jlnodoubles.php
@@ -17,6 +17,16 @@ class plgSystemJlnodoubles extends JPlugin
 {
 
     public static $noRedirect = null;
+    public static $isPro = null;
+
+    function __construct(&$subject, $config = array())
+    {
+        parent::__construct($subject, $config);
+        if(is_null(self::$isPro))
+        {
+            self::$isPro = ($this->allow($this->params->get('key'))) ? true : false;
+        }
+    }
 
     function onAfterRoute()
     {
@@ -25,27 +35,24 @@ class plgSystemJlnodoubles extends JPlugin
             return true;
         }
 
-        $option = JFactory::getApplication()->input->get('option', '', 'get');
-        $Itemid = JFactory::getApplication()->input->get('Itemid', 0, 'get', 'int');
-        $view = JFactory::getApplication()->input->get('view', 0, 'get');
-        $id = JFactory::getApplication()->input->get('id', 0, 'get', 'int');
+        $option = $app->input->get('option', '', 'get');
+        $Itemid = $app->input->get('Itemid', 0, 'get', 'int');
+        $view = $app->input->get('view', 0, 'get');
+        $id = $app->input->get('id', 0, 'get', 'int');
 
-        //$allGet=JFactory::getApplication()->input->get('get');
         $allGet = array(
             'Itemid' => $Itemid,
             'option' => $option,
             'view' => $view,
             'id' => $id
         );
-        //echo '<pre>allGet1::'.print_r($allGet,true).'</pre>';
-        //$allGet=JRequest::get('get');
 
-        $u = JURI::getInstance();
+        $u = JUri::getInstance();
         $currentLink = $u->toString(array('path'));
         $uir = $_SERVER['REQUEST_URI'];
 
 
-        $defvalue = array(
+        $defValue = array(
             array(
                 'var_name' => array(
                     'task',
@@ -67,7 +74,7 @@ class plgSystemJlnodoubles extends JPlugin
             )
         );
 
-        $componentsvars = $this->params->get('componentsvars', $defvalue);
+        $componentsvars = $this->params->get('componentsvars', $defValue);
 
         if (is_object($componentsvars)) {
             $componentsvars_array = array();
@@ -85,35 +92,36 @@ class plgSystemJlnodoubles extends JPlugin
             $componentsvars = $componentsvars_array;
         }
 
-
-        if (!isset($componentsvars[$option]["checkbox"])) {
+        if (!isset($componentsvars[$option]["checkbox"]))
+        {
             self::$noRedirect = true;
-            return;
+            return true;
         }
 
-        $cur_components = array(0, $option);
+        if (isset($componentsvars[$option]))
+        {
+            for ($i = 0; $i < sizeof($componentsvars[$option]["var_name"]); $i++)
+            {
+                $var_name = $componentsvars[$option]["var_name"][$i];
 
-        foreach ($cur_components as $comp) {
-            if (isset($componentsvars[$comp])) {
-                for ($i = 0; $i < sizeof($componentsvars[$comp]["var_name"]); $i++) {
-                    $var_name = $componentsvars[$comp]["var_name"][$i];
+                if (trim($componentsvars[$option]["var_value"][$i]))
+                {
+                    $var_values = array_map('trim', explode(',', $componentsvars[$option]["var_value"][$i]));
 
-                    if (trim($componentsvars[$comp]["var_value"][$i])) {
-
-                        $var_values = array_map('trim', explode(',', $componentsvars[$comp]["var_value"][$i]));
-
-                        foreach ($var_values as $vvalue) {
-                            if (isset($allGet[trim($var_name)])
-                                && $allGet[trim($var_name)] == trim($vvalue)
-                            ) {
-                                self::$noRedirect = true;
-                                return;
-                            }
+                    foreach ($var_values as $vvalue)
+                    {
+                        if (isset($allGet[trim($var_name)])
+                            && $allGet[trim($var_name)] == trim($vvalue)
+                        ) {
+                            self::$noRedirect = true;
+                            return true;
                         }
-                    } else if (isset($allGet[$var_name])) {
-                        self::$noRedirect = true;
-                        return;
                     }
+                }
+                else if (isset($allGet[$var_name]))
+                {
+                    self::$noRedirect = true;
+                    return true;
                 }
             }
         }
@@ -124,7 +132,8 @@ class plgSystemJlnodoubles extends JPlugin
                 if ($ag_name && $ag_value) $allGetArr[] = $ag_name . '=' . $ag_value;
             }
 
-            if (!$Itemid) {
+            if (!$Itemid)
+            {
                 $app = JFactory::getApplication();
                 $menus = $app->getMenu();
                 //$menus = &JApplication::getMenu('site', array());
@@ -133,25 +142,18 @@ class plgSystemJlnodoubles extends JPlugin
                 if ($Itemid) $allGetArr[] = 'Itemid=' . $Itemid;
             }
 
-            /*if (JRequest::getInt('start') > 0){
-                 $currentLink .= "?start=" . JRequest::getVar('start');
-            }
-            if (JRequest::getInt('limitstart') > 0){
-                 $currentLink .= "?limitstart=" . JRequest::getVar('limitstart');
-            }*/
-
             $redirectLink = JRoute::_('index.php?' . implode('&', $allGetArr));
             if ($redirectLink != $currentLink) {
                 $this->shRedirect($redirectLink);
             }
 
-        } else if ($option == "com_content") {
-            //$view=JRequest::getVar('view', '', 'get');
+        }
+        else if ($option == "com_content")
+        {
             if ($view == 'category') {
                 include_once(JPATH_SITE . '/components/com_content/helpers/route.php');
 
                 $catlink = JRoute::_(ContentHelperRoute::getCategoryRoute($allGet['id']));
-                //echo '<pre>allGet1::'.print_r($catlink,true).'</pre>';
 
                 if (JRequest::getInt('start') > 0) {
                     $catlink .= "?start=" . JRequest::getVar('start');
@@ -169,13 +171,12 @@ class plgSystemJlnodoubles extends JPlugin
 
     function onAfterRender()
     {
-
-        $app = JFactory::getApplication();
-        $homealias = $this->params->get('homealias', 'home');
-
-        if ($app->getName() != 'site') {
+        if (JFactory::getApplication()->getName() != 'site')
+        {
             return true;
         }
+
+        $homealias = $this->params->get('homealias', 'home');
         $buffer = JResponse::getBody();
         $regex = '#component/content/article/#m';
         $buffer = preg_replace($regex, $homealias . '/', $buffer);
@@ -185,7 +186,6 @@ class plgSystemJlnodoubles extends JPlugin
 
     public function onContentPrepare($context, &$article, &$params, $page = 0)
     {
-
         $real_link = JRequest::getURI();
         $original_link = '';
         $option = JFactory::getApplication()->input->get('option', '', 'get');
@@ -193,7 +193,8 @@ class plgSystemJlnodoubles extends JPlugin
         $layout = JFactory::getApplication()->input->get('layout', 0, 'get');
         $homealias = $this->params->get('homealias', 'home');
         if (self::$noRedirect) return;
-        if ($option == 'com_content') {
+        if ($option == 'com_content')
+        {
             switch ($view) {
                 case 'article':
                     $original_link = (isset($article->readmore_link)) ? $article->readmore_link : '';
@@ -233,7 +234,6 @@ class plgSystemJlnodoubles extends JPlugin
 
     function onAfterInitialise()
     {
-
         $application = JFactory::getApplication();
         $router = $application->getRouter();
         if ($router->getMode() == JROUTER_MODE_SEF) {
@@ -242,6 +242,72 @@ class plgSystemJlnodoubles extends JPlugin
         }
     }
 
+    private function allow($key)
+    {
+        $allowedHost = (empty($key)) ? 'localhost' : $key;
+        $allowedHost = explode('::', $allowedHost);
+        $allow = false;
+        foreach($allowedHost as $allowed) {
+            $allowed = $this->dsCrypt($allowed, true);
+            if(!empty($allowed)){
+                $allowed = explode('|', $allowed);
+                $site = (!empty($allowed[0])) ? $allowed[0] : 'localhost';
+                $extension = (!empty($allowed[1])) ? $allowed[1] : '';
+                $expireDate = (!empty($allowed[2])) ? $allowed[2] : '';
+                if(strpos($_SERVER['HTTP_HOST'], $site) !== false && $extension == 'plg_system_jlnodoubles'){
+                    $allow = true;
+                    break;
+                }
+            }
+        }
+        return $allow;
+    }
+
+    private function dsCrypt($input,$decrypt=false)
+    {
+        $o = $s1 = $s2 = array();
+        $basea = array('?','(','@',';','$','#',"]","&",'*');
+        $basea = array_merge($basea, range('a','z'), range('A','Z'), range(0,9) );
+        $basea = array_merge($basea, array('!',')','_','+','|','%','/','[','.',' ') );
+        $dimension=9;
+        for($i=0;$i<$dimension;$i++) {
+            for($j=0;$j<$dimension;$j++) {
+                $s1[$i][$j] = $basea[$i*$dimension+$j];
+                $s2[$i][$j] = str_rot13($basea[($dimension*$dimension-1) - ($i*$dimension+$j)]);
+            }
+        }
+        unset($basea);
+        $m = floor(strlen($input)/2)*2;
+        $symbl = $m==strlen($input) ? '':$input[strlen($input)-1];
+        $al = array();
+        for ($ii=0; $ii<$m; $ii+=2) {
+            $symb1 = $symbn1 = strval($input[$ii]);
+            $symb2 = $symbn2 = strval($input[$ii+1]);
+            $a1 = $a2 = array();
+            for($i=0;$i<$dimension;$i++) {
+                for($j=0;$j<$dimension;$j++) {
+                    if ($decrypt) {
+                        if ($symb1===strval($s2[$i][$j]) ) $a1=array($i,$j);
+                        if ($symb2===strval($s1[$i][$j]) ) $a2=array($i,$j);
+                        if (!empty($symbl) && $symbl===strval($s2[$i][$j])) $al=array($i,$j);
+                    }
+                    else {
+                        if ($symb1===strval($s1[$i][$j]) ) $a1=array($i,$j);
+                        if ($symb2===strval($s2[$i][$j]) ) $a2=array($i,$j);
+                        if (!empty($symbl) && $symbl===strval($s1[$i][$j])) $al=array($i,$j);
+                    }
+                }
+            }
+            if (sizeof($a1) && sizeof($a2)) {
+                $symbn1 = $decrypt ? $s1[$a1[0]][$a2[1]] : $s2[$a1[0]][$a2[1]];
+                $symbn2 = $decrypt ? $s2[$a2[0]][$a1[1]] : $s1[$a2[0]][$a1[1]];
+            }
+            $o[] = $symbn1.$symbn2;
+        }
+        if (!empty($symbl) && sizeof($al))
+            $o[] = $decrypt ? $s1[$al[1]][$al[0]] : $s2[$al[1]][$al[0]];
+        return implode('',$o);
+    }
 
     function build(&$router, &$uri)
     {
@@ -251,17 +317,17 @@ class plgSystemJlnodoubles extends JPlugin
         // Get the query data
         $query = $uri->getQuery(true);
 
-        if ($query['option'] == 'com_content') {
+        if ($query['option'] == 'com_content')
+        {
             $toUnset = array('showall', 'start', 'limitstart');
-            foreach ($toUnset as $tu) {
+            foreach ($toUnset as $tu)
+            {
                 if (isset($query[$tu]) && !$query[$tu]) unset($query[$tu]);
             }
         } else return;
-
 
         //Set query again in the URI
         $uri->setQuery($query);
         $uri->setPath($route);
     }
-
 }
