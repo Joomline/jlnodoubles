1c1618838970dc348f7289e2e72764e0fa366119
Arkadiy <a.sedelnikov@gmail.com>
2015-12-21 10:58:52 +0600

    Функция urlEncode от воланда.

--- helpers/helper.php
+++ helpers/helper.php
@@ -105,4 +105,11 @@ class JLNodoublesHelper{
             $o[] = $decrypt ? $s1[$al[1]][$al[0]] : $s2[$al[1]][$al[0]];
         return implode('',$o);
     }
+	
+	public function urlEncode($string) 
+	{
+		$entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
+		$replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
+		return str_replace($entities, $replacements, urlencode($string));
+	}
 }
